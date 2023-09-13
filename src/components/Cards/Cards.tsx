73e470cd1eb8b1547136254ece7486d366b69925
Delbert <delbert3luo@gmail.com>
2023-09-13 02:41:50 -0400

    dependency injection card

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,6 +1,9 @@
 import Grid from "../Grid/Grid";
+import IconContainer from "../IconContainer/IconContainer";
 import Card from "./Card/Card";
 import styles from "./Cards.module.scss";
+import vite from "../../assets/vite.svg";
+import react from "../../assets/react.svg";
 
 function Cards() {
   const array = [1, 2, 3, 4, 5, 6];
@@ -10,7 +13,16 @@ function Cards() {
         {array.map(() => {
           return (
             <Grid>
-              <Card />
+              <Card
+                imgSrc={vite}
+                description="CamelBak Insulated Podium Apple Pencil Chopsticks Popeyes iPhone has
+                  the and mouse"
+                technologyIcons={
+                  <IconContainer>
+                    <img src={react} height={30} width={30} />
+                  </IconContainer>
+                }
+              />
             </Grid>
           );
         })}
