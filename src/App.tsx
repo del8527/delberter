0efeead145d8730f2dcf959c65d4cb164d60dcca
Delbert <delbert3luo@gmail.com>
2023-09-12 23:55:31 -0400

    hero wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import styles from "./App.module.scss";
 
 import Cards from "./components/Cards/Cards";
 import HackedText from "./components/HackedText/HackedText";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className={styles.App}>
-
+      <Hero />
       <Cards />
       <HackedText text="PLACEHOLDER" />
     </div>
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import styles from "./Hero.module.scss";
+
+function Hero() {
+  return (
+    <div className={styles.Hero}>
+      <div>ad</div>
+    </div>
+  );
+}
+
+export default Hero;
