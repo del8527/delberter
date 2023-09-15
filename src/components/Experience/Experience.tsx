e529cada3125979d10dfadba352c666cf388d34d
Delbert <delbert3luo@gmail.com>
2023-09-15 00:34:47 -0400

    experience component

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -5,6 +5,7 @@ import styles from "./Experience.module.scss";
 function Experience() {
   return (
     <div className={styles.Experience}>
+        <h1 className={styles.Experience__header}>Experience</h1>
       <Cards cards={ExperienceCards} />
     </div>
   );
